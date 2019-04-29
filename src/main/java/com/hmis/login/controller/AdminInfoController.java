e5f8c60ef7ae608e2f390248bb743feeab286c59
xishen.yan <xishen.yan@hand-china.com>
2019-04-29 18:06:16 +0800

    管理员登录-就剩验证码没做了

--- src/main/java/com/hmis/login/controller/AdminInfoController.java
+++ src/main/java/com/hmis/login/controller/AdminInfoController.java
@@ -29,31 +29,35 @@ public class AdminInfoController {
     @Autowired
     private AdminInfoService adminInfoService;
 
+    /**
+     * 管理员登录校验
+     * @param adminInfo 传入的Json格式数据，用户名 密码 验证码
+     * @return pojoMsg  返回Json格式数据
+     */
     @RequestMapping(value = "/adminLogin")
     @ResponseBody
-    public PojoMsg adminLogin(@RequestBody AdminInfo adminInfo){
-        List<AdminInfo> resultAdminInfo = adminInfoService.adminLogin(adminInfo);
+    public PojoMsg adminLogin(@RequestBody AdminInfo adminInfo,HttpServletRequest request){
+        List<AdminInfo> adminInfoList = adminInfoService.adminLogin(adminInfo);
         PojoMsg pojoMsg = new PojoMsg();
-        if (resultAdminInfo.size() == 1){
+        if (adminInfoList.size() == 1){
             pojoMsg.setSuccess(true);
             pojoMsg.setMsg("登录成功！");
+
+            //将用户名放入session中，在登录后的主页面可以从session中获取，显示在右上角
+            HttpSession session = request.getSession();
+            String userName = adminInfoList.get(0).getUserName();
+            session.setAttribute("userName",userName);
+
+            //执行成功后返回给登录页面的数据，实际上拿到这些数据也不用，所以不放入这些信息也行
             int count = 0;
-            for(AdminInfo adminInfo1:resultAdminInfo){
-                pojoMsg.add(String.valueOf(count++),adminInfo1);
+            for(AdminInfo adminInfo_elem : adminInfoList){
+                pojoMsg.add(String.valueOf(count++),adminInfo_elem);
             }
             return pojoMsg;
-
-//            HttpSession session=request.getSession();
-//            session.setAttribute("resultAdminInfo", resultAdminInfo);
-//            return "views/admin/main.jsp";
         }else{
             pojoMsg.setSuccess(true);
             pojoMsg.setMsg("用户名或密码错误！");
             return pojoMsg;
-
-//            request.setAttribute("adminInfo", adminInfo);
-//            request.setAttribute("errorMsg", "用户名或密码错误！");
-//            return "redirect:/login.jsp";
         }
     }
 
